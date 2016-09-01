9aad8eac9ccb1cab9aafa52d0c7efb09befd3d75
Adam Miskiewicz <adam@exponentjs.com>
2016-09-01 16:03:47 -0700

    Remove unneeded log.

--- makeMeADir.js
+++ makeMeADir.js
@@ -2,6 +2,5 @@ import path from 'path';
 import mkdirp from 'mkdirp';
 
 export default function makeMeADir() {
-   console.log(path.join(__dirname));
    mkdirp.sync(path.join(__dirname, 'some-other-test-dir', 'some-test-dir'));
 }
